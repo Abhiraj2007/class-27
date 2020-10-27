42b33eda7ef1a0e9c656465705efb7e3d2cc3eea
Abhiraj2007 <72135959+Abhiraj2007@users.noreply.github.com>
2020-10-27 16:52:46 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,22 @@
+class chain{
+
+constructor(bodyA,bodyB){
+var options={
+    bodyA=bodyA,
+    bodyB=bodyB,
+    stiffness=0.04,
+    length=10
+}
+this.chain=constraint.create(options);
+World.add(world,this.chain);
+}
+
+display(){
+ pointA=this.chain.bodyA.position;
+ pointB=this.chain.bodyB.position;
+ line(pointA.x,pointA.y,pointB.x,pointB.y)
+}
+
+
+}
+
